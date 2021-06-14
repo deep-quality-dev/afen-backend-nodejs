640b971f177e12bb26156b578e364aec978fb227
deep-quality-dev <hungrywarrior081@gmail.com>
2021-06-14 05:32:48 -0400

    Update NFT Schema

--- models/nft.js
+++ models/nft.js
@@ -7,29 +7,6 @@ const NftSchema = new Schema(
       type: String,
       required: true,
     },
-    tag: {
-      type: String,
-      required: true,
-    },
-    likes: {
-      type: Number,
-      default: 0,
-    },
-    liked_by: {
-      type: Array,
-      default: [],
-    },
-    comments: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Comment',
-      },
-    ],
-    posted_by: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
   },
   {
     timestamps: true,
